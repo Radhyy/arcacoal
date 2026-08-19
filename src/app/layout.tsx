47c7b69025110931db1da8ce6c856889594ce3fa
Radhyy <raaakb87@gmail.com>
2026-08-19 09:13:49 +0700

    feat: add google analytics (gtag.js) to layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -136,6 +136,18 @@ j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
           }}
         />
         {/* End Google Tag Manager */}
+        {/* Google tag (gtag.js) */}
+        <script async src="https://www.googletagmanager.com/gtag/js?id=G-S5G4SJT4J3"></script>
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', 'G-S5G4SJT4J3');
+            `,
+          }}
+        />
         <script
           type="application/ld+json"
           dangerouslySetInnerHTML={{ __html: JSON.stringify(jsonLd) }}
